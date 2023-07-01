cb136b77964e9773d80f5c835fd2fabbcb510bae
triveni2004 <triveninidadavolu@gmail.com>
2023-07-01 17:11:24 +0530

    assignment4

---
+++ eve.js
@@ -0,0 +1,25 @@
+function removeItem(e) {
+    e.preventDefault();
+    if (e.target.classList.contains("delete")) {
+        if (confirm("Are you Sure?")) {
+            let li = e.target.parentNode;
+            items.removeChild(li);
+            document.getElementById("lblsuccess").innerHTML
+                = "Text deleted successfully";
+  
+            document.getElementById("lblsuccess")
+                        .style.display = "block";
+  
+            setTimeout(function() {
+                document.getElementById("lblsuccess")
+                        .style.display = "none";
+            }, 3000);
+        }
+    }
+    if (e.target.classList.contains("edit")) {
+        document.getElementById("item").value =
+            e.target.parentNode.childNodes[0].data;
+        submit.value = "EDIT";
+        editItem = e;
+    }
+}  
